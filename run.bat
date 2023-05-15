20d3bf783cb64f599a472daaf7ee03d3f6aef2a2
Peter <Peter_Betlem@hotmail.com>
2023-05-15 11:27:25 +0200

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 cd %~dp0
-CALL conda activate
-CALL mamba activate geomod
+CALL conda activate %name%
 CALL jupyter book build .
 CALL ghp-import -n -p -f _build/html
+CALL conda env export -n %name% > %name%.yaml
